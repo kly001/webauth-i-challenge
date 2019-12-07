7b755ae5136847204c73615166d4b61d976f3792
kly001 <karenly1961@gmail.com>
2019-12-07 16:30:26 -0500

    auth-router

---
+++ auth/auth-router.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req,res) => {
+    let user = req.body;
+
+    Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+})
+
+router.post('./login',(req,res) => {
+
+})
+
+module.exports = router
