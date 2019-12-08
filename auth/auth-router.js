f8bce5322fe45a3180556af569511c9970df89b5
kly001 <karenly1961@gmail.com>
2019-12-07 23:01:40 -0500

    register/login working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -17,7 +17,23 @@ router.post('/register', (req, res) => {
     });
 })
 
-router.post('./login',(req,res) => {
+router.post('/login',(req,res) => {
+  let {username,password} = req.body
+
+  Users.findBy({username})
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)) {
+      // check to see if password is valid
+
+      res.status(200).json({message:`Congratulations, ${user.username}. You are now logged in !`})
+    } else {
+      res.status(401).json({message:'Invalid credentials'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 
 })
 
