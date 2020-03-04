6be8761c194b1bbd902805e034e08ef86d346b39
kly001 <karenly1961@gmail.com>
2020-03-04 00:35:33 -0500

    ...

--- auth/requiresAuth.js
+++ auth/requiresAuth.js
@@ -2,23 +2,7 @@ const Users = require('../users/users-model.js');
 const bcrypt = require('bcryptjs');
 
 module.exports = (req, res,next)=> {
-    const { username, password } = req.headers;
 
-    // if(!(username && password)) {
-    //   res.status(401).json({message:"Invalid Credentials"})
-    // } else {
-    //   Users.findBy({username})
-    //   .first()
-    //   .then(user => {
-    //     if(user && bcrypt.compareSync(password, user.password)){
-    //       next()
-    //     } else {
-    //       res.status(401).json({message:"Invalid Credentials"})
-    //     }
-    //   })
-    //   .catch((err) => { res.status(500).json({ message: err }) })
-    //   }
-    // }
     if (req.session && req.session.user) {
       next();
      } else {
